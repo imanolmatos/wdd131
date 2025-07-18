b21b0a4c69453d296ecc7c5e3f1ed5b60cdf7f82
imanolmatos <imanolmatos007@gmail.com>
2025-07-18 11:12:07 -0400

    comitted

---
+++ scripts/getdate.js
@@ -0,0 +1,10 @@
+let current = document.querySelector("#currentyear");
+let lastM = document.querySelector("#last");
+
+let today = new Date();
+let lastModif = new Date(document.lastModified);
+let formatedLast = lastModif.toLocaleString("en-US",{timeZoneName:"short"});
+
+current.innerHTML = today.getFullYear();
+lastM.innerHTML = formatedLast;
+
